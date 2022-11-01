340e870e79db0b851485e9d35dd2e1be7df3eacc
Emmanuel Belini <beliniemmanuel@gmail.com>
2022-11-01 09:51:29 -0300

    mudanças no app.py e assets criado

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+class Usuario:
+    def __init__(self,nome,email,senha):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
 
 app = Flask(__name__)
 
@@ -9,5 +15,13 @@ def home():
 @app.route('/login')
 def login():
     return render_template('cadastro.html', titulo_login='Cadastro')
+
+@app.route('/criar')
+def criar():
+    nome = request.form['nome']
+    email = request.form['email']
+    senha = request.form['senha']
+    usuario = Usuario(nome, email, senha)
+
 
 app.run()
