a3d7f7b13050fcf8375dd9b7f71083db9b7743d2
Emmanuel Belini <beliniemmanuel@gmail.com>
2022-11-06 01:53:21 -0300

    static added

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-from flask import Flask, render_template, request
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
-#database
-cred = credentials.Certificate('blacktie-2a2a9-firebase-adminsdk-59nbn-f6c28ab314.json')
-app = firebase_admin.initialize_app(cred)
-db = firestore.client()
+from flask import Flask, render_template, request, re
 
 
 #classe para o formulario de cadasto
